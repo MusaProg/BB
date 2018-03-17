dfb0614b2d1e070a6688ebe81f9211200b3afc46
MusaProg <37460581+MusaProg@users.noreply.github.com>
2018-03-17 03:52:27 +0300

    Delete Test1.php

--- Test1.php
+++
@@ -1,9 +0,0 @@
-<?php
-	function Get()
-	{
-		$Users_DB = new mysqli("localhost", "root", "", "Users_Main_Information");
-		$result = $Users_DB->query("SELECT `id` FROM `Information` WHERE `id` > 1
-		Limit 2");
-		return $result;
-	}
-?>
